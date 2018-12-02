86257f6f09211f9cc6aa0da0093c98c1c5ede853
ACCIONLABS\AL2342 <vaibhav.katariya@accionlabs.com>
2018-12-02 20:05:01 +0530

    update file structur

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import CallRecordedList from './CallRecordedList';
+import Settings from './Settings';
+import Home from './Home';
+import About from './About';
+import ScreenNavigation from './ScreenNavigation';
+
+export { Home, Settings, CallRecordedList, About, ScreenNavigation };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import CallRecordedList from './components/CallRecordedList';
-import Settings from './components/Settings';
-import Home from './components/Home';
-import About from './components/About';
-import ScreenNavigation from './components/ScreenNavigation';
+import App from './Routes';
 
-export { Home, Settings, CallRecordedList, About, ScreenNavigation };
+export { App };
